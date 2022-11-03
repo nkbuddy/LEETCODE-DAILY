fbb67a10b73e0257e40aca4c16f8b0580f28e9a4
Alan Chen <49331641+nkbuddy@users.noreply.github.com>
2022-11-03 18:32:42 -0400

    Time: 1023 ms (57.04%), Space: 0B (100.00%) - LeetHub

---
+++ 2072-the-winner-university/2072-the-winner-university.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT(
+    CASE
+    WHEN (SELECT COUNT(student_id) FROM NewYork WHERE score >= 90) > (SELECT COUNT(student_id) FROM California WHERE score >= 90) THEN 'New York University'
+    WHEN (SELECT COUNT(student_id) FROM NewYork WHERE score >= 90) < (SELECT COUNT(student_id) FROM California WHERE score >= 90) THEN 'California University'
+    ELSE 'No Winner' END
+    ) AS 'winner'
