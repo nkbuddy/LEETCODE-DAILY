b3dee5063f293982c91fea3b6f5f01a859e40296
Alan Chen <49331641+nkbuddy@users.noreply.github.com>
2022-11-13 14:16:32 -0500

    Time: 1191 ms (80.03%), Space: 0B (100.00%) - LeetHub

--- 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
+++ 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
@@ -1,12 +1,4 @@
 # Write your MySQL query statement below
-WITH temp_reg AS(
-SELECT
-    contest_id, COUNT(user_id) AS 'total_id'
-FROM
-    Register
-GROUP BY
-    contest_id
-)
 SELECT
     contest_id, ROUND(COUNT(user_id)/(SELECT COUNT(*) FROM Users) *100, 2) AS 'percentage'
 FROM
