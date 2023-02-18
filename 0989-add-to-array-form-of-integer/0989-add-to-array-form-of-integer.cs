47917d48e14067426c94e5fb071eac36afcab973
Alan Chen <49331641+nkbuddy@users.noreply.github.com>
2023-02-18 12:52:18 -0500

    Time: 238 ms (7.46%), Space: 53.5 MB (41.99%) - LeetHub

--- 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.cs
+++ 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.cs
@@ -1,10 +1,5 @@
 public class Solution {
     public IList<int> AddToArrayForm(int[] num, int k) {
-        double converted_num = double.Parse(string.Join("",num));
-        Console.WriteLine(converted_num);
-        converted_num += k;
-        Console.WriteLine(converted_num);
-        int[] ans = converted_num.ToString().Select(c => (int)Char.GetNumericValue(c)).ToArray();
         return (BigInteger.Parse(string.Join("", num.Select(x => x))) + k).ToString().Select(x => int.Parse(x.ToString())).ToList();
     }
 }
