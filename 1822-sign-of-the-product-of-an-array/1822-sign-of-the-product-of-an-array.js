5b26737899a450431ea7e66fc3144be78f7ed185
Alan Chen <49331641+nkbuddy@users.noreply.github.com>
2023-05-02 08:38:47 -0400

    Time: 64 ms (40.83%), Space: 42.3 MB (87.02%) - LeetHub

---
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var arraySign = function(nums) {
+    let count = 0;
+    for(let i =0 ;i<nums.length;i++){
+        if(nums[i]==0){
+            return 0
+        }else if(nums[i]<0){
+            count++;
+        }
+    }
+    return count%2==1?-1:1;
+};
