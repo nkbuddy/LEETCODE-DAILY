a38775293c0047cc79fd03a5dccbf52e7111a961
Alan Chen <49331641+nkbuddy@users.noreply.github.com>
2023-06-15 16:18:13 -0400

    Time: 3310 ms (7.13%), Space: 52.6 MB (50.66%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.ts
+++ 0189-rotate-array/0189-rotate-array.ts
@@ -2,5 +2,14 @@
  Do not return anything, modify nums in-place instead.
  */
 function rotate(nums: number[], k: number): void {
-    nums.unshift(...nums.splice(-(k % nums.length)));
+    k %= nums.length;//3
+let temp:number,previous:number;
+for(let i =0;i<k;i++){
+  previous = nums[nums.length-1];
+  for(let j = 0; j < nums.length;j++){
+    temp = nums[j];
+    nums[j]=previous;
+    previous = temp;
+  }
+}
 };
