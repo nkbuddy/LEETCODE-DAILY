60e6352bfba74210f92b9f1f20ff86a6199f29bd
Alan Chen <49331641+nkbuddy@users.noreply.github.com>
2023-06-16 15:40:55 -0400

    Time: 112 ms (65.67%), Space: 52.3 MB (68.99%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.ts
+++ 0189-rotate-array/0189-rotate-array.ts
@@ -3,17 +3,17 @@
  */
 function rotate(nums: number[], k: number): void {
     k =k % nums.length;
-    let count:number =0;
-    for(let start = 0;count<nums.length;start++){
-        let current = start;
-        let prev = nums[start];
-        do{
-            let next:number = (current + k) % nums.length;
-            let temp:number = nums[next];
-            nums[next] = prev;
-            prev = temp;
-            current = next;
-            count++;
-        }while(start!==current);
-    }
+    reverse(nums,0,nums.length-1);
+    reverse(nums,0,k-1);
+    reverse(nums,k,nums.length-1);
 };
+
+function reverse(nums:number[],start:number,end:number){
+    while(start<end){
+        let temp:number = nums[start];
+        nums[start] = nums[end];
+        nums[end] = temp;
+        start++;
+        end--;
+    }
+}
