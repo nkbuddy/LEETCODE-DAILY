37c84841781764a01bba0e42f19274ea42bdb059
Alan Chen <49331641+nkbuddy@users.noreply.github.com>
2023-06-28 11:47:29 -0400

    Time: 115 ms (60.69%), Space: 52.2 MB (74.75%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.ts
+++ 0189-rotate-array/0189-rotate-array.ts
@@ -3,8 +3,8 @@
  */
 function rotate(nums: number[], k: number): void {
     let a:number[]=[];
-    for(let i = 0;i<nums.length;i++){
-        a[(i+k)%nums.length]=nums[i];
+    for(let i=0;i<nums.length;i++){
+        a[(k+i)%nums.length]=nums[i];
     }
     for(let i=0;i<nums.length;i++){
         nums[i]=a[i];
